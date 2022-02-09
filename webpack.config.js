3adc01378a2ffd7eef0bad313a4ad0f26bc42b47
Lucas Dias <lucasmg37@gmail.com>
2022-02-09 20:53:29 -0300

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './public/index.html',
+  })],
+  mode: 'development',
+}
