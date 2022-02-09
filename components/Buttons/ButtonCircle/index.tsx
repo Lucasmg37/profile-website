5f168c91b73fc9848f838e539c12940bff33b87b
Lucas Dias <lucasmg37@gmail.com>
2022-02-09 18:07:04 -0300

    fix eslint

--- components/Buttons/ButtonCircle/index.tsx
+++ components/Buttons/ButtonCircle/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from './styles';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> { }
 
 const ButtonCircle: React.FC<ButtonProps> = ({ children, ...rest }: ButtonProps) => {
